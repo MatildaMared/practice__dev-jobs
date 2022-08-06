f50f958f9a45f537d0da63b0f355b7238a26995c
Matilda Mared <matildamared@live.se>
2022-08-06 17:40:23 +0200

    add breakpoints

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
+
 </script>
 
 <header>
-	<h1>Header</h1>
+    <h1>Header</h1>
 </header>
 
-<style>
-	header {
-		display: flex;
-		justify-content: space-between;
-	}
+<style lang="scss">
+  header {
+    display: flex;
+    justify-content: space-between;
+    background: url("/images/mobile/bg-pattern-header.svg") no-repeat center;
+    background-size: cover;
+    padding: 32px;
+  }
 </style>
