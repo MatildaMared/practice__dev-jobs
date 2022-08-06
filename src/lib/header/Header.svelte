abb5cbcbc4441d1df5801e275e43691e0c6ed2ec
Matilda Mared <matildamared@live.se>
2022-08-06 18:49:23 +0200

    add background url for header

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -7,11 +7,23 @@
 </header>
 
 <style lang="scss">
-  header {
-    display: flex;
-    justify-content: space-between;
-    background: url("/images/mobile/bg-pattern-header.svg") no-repeat center;
-    background-size: cover;
-    padding: 32px;
-  }
+    @use "../../styles/breakpoints.scss" as *;
+
+    header {
+        display: flex;
+        justify-content: space-between;
+        background: url("/images/mobile/bg-pattern-header.svg") no-repeat center;
+        background-size: cover;
+        padding: 32px;
+
+        @media (min-width: $breakpoint-tablet-portrait) {
+            background: url("/images/tablet/bg-pattern-header.svg") no-repeat bottom;
+            background-size: cover;
+        }
+
+        @media (min-width: $breakpoint-laptop) {
+            background: url("/images/desktop/bg-pattern-header.svg") bottom left;
+            background-size: cover;
+        }
+    }
 </style>
