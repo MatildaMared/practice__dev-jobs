f20485ad3c238f8b762143fca8fbf59fbf47711a
Matilda Mared <matildamared@live.se>
2022-08-09 08:14:55 +0200

    add dynamic route for jobs

---
+++ src/routes/jobs/[id].svelte
@@ -0,0 +1,16 @@
+<script context="module">
+	export async function load ({params}) {
+		const {id} = params;
+		return {
+			props: {id}
+		};
+	}
+</script>
+
+<script lang="ts">
+	export let id;
+</script>
+
+<h1>{id}</h1>
+
+<style lang="scss"></style>
